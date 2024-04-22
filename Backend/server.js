4e8f816c698bdd2b5647e94407b2b99ec8ddd14c
Mustafa Ahmed <mustafabukhari333@gmail.com>
2024-04-22 10:47:45 +0500

    Project Completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,27 @@
-import authRoutes from './routes/authRoutes.js';
-import connectDataBase from './db/dataBaseConnection.js';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
-import express from 'express';
-import messageroute from './routes/messageRoute.js';
-import userRoute from './routes/userRoutes.js';
-import { app, server } from './socket/socket.io.js';
+import authRoutes from "./routes/authRoutes.js";
+import connectDataBase from "./db/dataBaseConnection.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import messageroute from "./routes/messageRoute.js";
+import path from "path";
+import userRoute from "./routes/userRoutes.js";
+import { app, server } from "./socket/socket.io.js";
 
 dotenv.config();
 const Port = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageroute);
 app.use('/api/users', userRoute);
+app.use(express.static(path.join(__dirname, 'Frontend/dist')));
+
+app.get('*', (req, res) => {
+	res.send(path.join(__dirname, 'Frontend/dist/index.html'));
+});
 
 server.listen(Port, () => {
 	connectDataBase();
